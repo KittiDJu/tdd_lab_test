8c4e8552ae86098ef4fa3aa599a6458aaee412fa
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-22 14:32:34 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,13 +3,18 @@ from mangum import Mangum
 
 app = FastAPI()
 
+names = ""
+
 @app.get("/callname/{name}")
 def get_name(name: str):
-    global names = name
+    global names
+    names = name
     return {"hello": names}
+
 handler = Mangum(app)
 
 @app.post("/callname")
 def post_name():
     return {"hello": names}
+
 handler = Mangum(app)
