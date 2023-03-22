77619a21a80d6b2cc12c018cd87d167266be6981
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-22 13:42:57 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,11 +4,11 @@ from app.main import app
 client = TestClient(app)
 
 def test_get_name():
-    response = client.get("/{name}")
+    response = client.get("/callname/{name}")
     assert response.status_code == 200
     assert response.json() == {"hello": "{name}"}
     
 def test_post_name():
-    response = client.post("/{name}")
+    response = client.post("/callname")
     assert response.status_code == 200
     assert response.json() == {"hello": "{name}"}
