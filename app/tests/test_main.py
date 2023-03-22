1f2379b6fe7b8acfc0fdc1dd9ce36c4efb6927b0
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-22 13:24:32 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,6 +4,6 @@ from app.main import app
 client = TestClient(app)
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/{name}")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"hello": name}
