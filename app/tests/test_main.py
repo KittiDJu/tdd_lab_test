4eee5c5d218f41a07629ef54fa42062031eff700
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-22 14:26:31 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,6 +4,6 @@ from app.main import app
 client = TestClient(app)
 
 def test_get_name():
-    response = client.get("/callname/{name}")
-    assert response.status_code == 200
-    assert response.json() == {"hello": "{name}"}
+    global responseget = client.get("/callname/{name}")
+    assert responseget.status_code == 200
+    assert responseget.json() == {"hello": "{name}"}
